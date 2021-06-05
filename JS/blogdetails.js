2920913c3ceb39f8fd899705d5096835268c4206
mrh1992 <71317905+mrh1992@users.noreply.github.com>
2021-06-05 11:52:48 +0200

    added modal image to blogdetails

--- JS/blogdetails.js
+++ JS/blogdetails.js
@@ -6,19 +6,13 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-console.log(id);
-
 const url ="https://www.hollundflowerpower.one/wp-json/wp/v2/posts/" + id + "?_embed";
-
-console.log(url);
 
 async function fetchBlogPost() {
 
     try {
         const response = await fetch(url);
         const details = await response.json();
-
-        console.log(details);
 
 
         const convertDate = new Date (details.date).toLocaleString("en-GB", {day: "numeric", month: "long", year: "numeric",});
@@ -29,8 +23,30 @@ async function fetchBlogPost() {
                                             <p class="posted-date">Posted: ${convertDate}<p>
                                             <p>Written by: ${details._embedded.author[0].name}</p>
                                         </div>
-                                        <div class="blog-content">${details.content.rendered}</div>
+                                        <div class="blog-content">${details.content.rendered}
+                                            <img src=${details._embedded['wp:featuredmedia']['0'].source_url} alt="${details.name}" class="blog-img">
+                                            <div class="modal-container">
+                                                <span class="close">&times;</span>
+                                                <img class="modal-image">
+                                            </div>
+                                        </div>
                                     </div>`;
+        //Modal Image
+
+        const modal = document.querySelector(".modal-container");
+        const img = document.querySelector(".blog-img");
+        const modalImage = document.querySelector(".modal-image");
+
+        img.onclick = function() {
+            modal.style.display = "block";
+            modalImage.src = this.src;
+        };
+
+        modal.onclick = function() {
+            modal.style.display = "none";
+        }
+
+                        
     } 
     catch(error) {
         console.log();
